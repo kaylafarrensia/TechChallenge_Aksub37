5353d1ccd2cfaff41c512dd582023b6f09905e62
Kayla Farrensia <kaylafarrensia@gmail.com>
2026-02-18 22:45:50 +0700

    togglelightanddarkmode

---
+++ KaylaFarrensia_TechChallenge_ToggleLightandDarkMode_02/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  // Runs code when HTML ready
+  // When there is change on switch (button clicked) the function runs
+  $("#switch").on("change", function () {
+    // Toggles the CSS class
+    // Adds class if nonexistent, removes if it exists
+    $("body").toggleClass("darkMode");
+
+    // Update the text label based on the state: dark mode or light mode
+    // If checked (checkbox bool status true) then show dark mode, otherwise light mode
+    $("#modeText").text($(this).is(":checked") ? "Dark Mode" : "Light Mode");
+  });
+});
