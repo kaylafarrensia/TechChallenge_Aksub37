5ce8fa180db2bca254274a5f36017b14a767768c
Kayla Farrensia <kaylafarrensia@gmail.com>
2026-03-07 20:59:23 +0700

    week 1

---
+++ FE_Training_W1/src/main.js
@@ -0,0 +1,13 @@
+// Connects the logic from register and login and places it into one entry point
+import "./register.js";
+import "./login.js";
+
+export let users = [
+  {
+    email: "dummy@gmail.com",
+    password: "123!@ABC",
+  },
+];
+
+// Success message in console
+console.log("Successful vite implementation!");
